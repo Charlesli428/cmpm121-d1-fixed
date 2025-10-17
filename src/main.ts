0f430592b2a51711450786d85cf272d690eb14b9
Charlesli428 <cli310@ucsc.edu>
2025-10-17 11:12:03 +0000

    Step 2 complete: Added working counter

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ document.body.innerHTML = "";
 const app = document.createElement("div");
 document.body.append(app);
 
+//Step 1
 const button = document.createElement("button");
 button.textContent = "🔥";
 app.append(button);
+
+//Step 2
+let counter: number = 0;
+const counterDiv = document.createElement("div");
+counterDiv.textContent = `Total Aura: ${counter}`;
+app.append(counterDiv);
+button.addEventListener("click", () => {
+  counter += 1;
+  counterDiv.textContent = `Total Aura: ${counter}`;
+});
