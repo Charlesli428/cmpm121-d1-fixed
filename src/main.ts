72a9ca33c66d5673b23b1762174df6cb77c1fa2f
Charlesli428 <cli310@ucsc.edu>
2025-10-17 11:06:29 +0000

    Step 1 complete: added fire emoji button

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+document.body.innerHTML = "";
+const app = document.createElement("div");
+document.body.append(app);
+
+const button = document.createElement("button");
+button.textContent = "🔥";
+app.append(button);
